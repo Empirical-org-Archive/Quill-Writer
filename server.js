488529d84854d92e114acab2ed40c98c470ce701
Quinn Shanahan <q.shanahan@gmail.com>
2014-06-06 15:48:31 -0400

    port

--- server.js
+++ server.js
@@ -129,5 +129,5 @@ process.on('uncaughtException', function (err) {
 
 ////////////
 
-server.listen(3000);
-console.log('Listening on port 3000');
+server.listen(process.env.PORT);
+console.log('Listening on port '+ process.env.PORT);
