16c5a0ba8109feec628556cc674e04985362af4b
wlaurance <w.laurance@gmail.com>
2014-06-27 13:22:35 -0400

    Serve up files using express static for now
    See #33

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.use('/', routes);
 app.use('/users', users);
