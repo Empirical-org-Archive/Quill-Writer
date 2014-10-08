7b07e85f72edaeda54a7f78c196c1c0f79b0713a
wlaurance <w.laurance@gmail.com>
2014-10-08 10:30:33 -0400

    Use localStoreageService to store currentUser
    See #98

--- src/common/services/user/index.js
+++ src/common/services/user/index.js
@@ -1,12 +1,14 @@
 angular.module('sf.services.user', [])
 
-  .service('User', function() {
+  .service('User', function(localStorageService) {
     var user = this;
 
     user.currentUser = null;
+    user.currentUserKey = 'currentUser';
 
     user.setCurrentUser = function(newUser) {
       user.currentUser = newUser;
+      localStorageService.set(user.currentUserKey, newUser);
     };
   })
 
