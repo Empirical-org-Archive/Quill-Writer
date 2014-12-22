bc549de882e5aa8621277f4b4c318afd90d52566
wlaurance <w.laurance@gmail.com>
2014-12-22 15:01:05 -0500

    Game module notes

--- src/app/game/index.js
+++ src/app/game/index.js
@@ -4,6 +4,13 @@ var game = require('./../../common/services/game/');
 
 var fs = require('fs');
 
+/*
+ * sf.game is a module with _a lot_ going on. We should split the individual components
+ * into directives with their own controllers, services etc. Most of the action is in the game.
+ * TODO This file should become a simple module that loads all of the dependent directives,
+ * modules, services etc and glues them together nicely.
+ */
+
 angular.module('sf.game', [
     'ui.router',
     profanityFilter,
