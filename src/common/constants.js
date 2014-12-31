694946d431f9febcaaf7030af8763b64e6e7873f
Kristopher Kelly <kristopherbkelly@gmail.com>
2014-12-31 14:48:22 -0500

    Temporary env hack for development

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,9 +2,12 @@ angular.module("sf.constants", [])
 
   .constant("appName", "Quill Writer")
   .constant("tagLine", "Learn by Writing Together")
-  .constant("baseFbUrl", "https://quill-writer.firebaseio.com/")
-  .constant("empiricalBaseURL", "https://staging.quill.org/api/v1")
+  // TODO: Figure out how to separate environments
 
+  .constant("baseFbUrl", "https://quill-writer.firebaseio.com/")
+
+  // .constant("empiricalBaseURL", "https://staging.quill.org/api/v1")
+  .constant("empiricalBaseURL", "http://localhost:3000/api/v1")
 ;
 
 module.exports = 'sf.constants';
